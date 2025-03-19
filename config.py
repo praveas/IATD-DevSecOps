e935850cf3e64649304e567ac243ed14a83b939a
Prabesh Pokharel <praveas@gmail.com>
2025-03-19 15:40:08 +1100

    Load database differently

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ from constants import APPLICATION_JSON, INVALID_TOKEN
 
 vuln_app = connexion.App(__name__, specification_dir='./openapi_specs')
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(vuln_app.app.root_path, 'database/database.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///./database/database.db'
 vuln_app.app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 vuln_app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 vuln_app.app.config['SECRET_KEY'] = os.getenv("VULVAPP_SECRETKEY")
 # start the db
 db = SQLAlchemy(vuln_app.app)
-vuln_conn = sqlite3.connect(os.path.join(vuln_app.app.root_path, 'database/database.db') , check_same_thread=False)
+vuln_conn = sqlite3.connect('./database/database.db' , check_same_thread=False)
 
 vuln_app.add_api('openapi3.yml')
